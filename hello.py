4da44aba5d94590915932dadfc145682ce54c4ee
陌路拾荒 <xieqiudongv5@163.com>
2019-05-29 09:45:26 +0800

    1

--- hello.py
+++ hello.py
@@ -41,19 +41,19 @@ import string
 # x = "the number is {num:20}".format(num=45555)
 # x = "the number is '{num:20.2}'".format(num="gdwugeugw")
 
-# x = '{:015.3f}\n'.format(math.pi)  # 0来填充数字
-# y = '{:<15.3f}\n'.format(math.pi)  # 左对齐
-# z = '{:>15.3f}\n'.format(math.pi)  # 右对齐
-# n = '{:@>15.3f}'.format(math.pi)  # 右对齐，指定符号填充
-# print(x + y + z + n)
+x = '{:015.3f}\n'.format(math.pi)  # 0来填充数字
+y = '{:<15.3f}\n'.format(math.pi)  # 左对齐
+z = '{:>15.3f}\n'.format(math.pi)  # 右对齐
+n = '{:@>15.3f}'.format(math.pi)  # 右对齐，指定符号填充
+print(x + y + z + n)
 
 # print(string.digits)
 
-x = {"name": "xqd",
-     "age": "26",
-     "sex": "man"}
-# del x["hg"] #删除字典项 如果没有这个key的话会报错
-# x.pop("name")  #也是删除字典项，如果没有这个key的话会报错
-print(x.items())  # 转换为列表
-print(x.keys())  # 获取所有键的列表
-print(x.values())  # 获取所有值的列表
+# x = {"name": "xqd",
+#      "age": "26",
+#      "sex": "man"}
+# # del x["hg"] #删除字典项 如果没有这个key的话会报错
+# # x.pop("name")  #也是删除字典项，如果没有这个key的话会报错
+# print(x.items())  # 转换为列表
+# print(x.keys())  # 获取所有键的列表
+# print(x.values())  # 获取所有值的列表
